955653050b3e2e12d0495807f5b97f33f0521c16
Isabelle Villasenor <villasenori@outlook.com>
2021-02-12 09:22:30 -0700

    Add Search Index Template

--- spec/features/avatar_search_spec.rb/index_spec.rb
+++ spec/features/avatar_search_spec.rb/index_spec.rb
@@ -1,1 +1,13 @@
 require 'rails_helper'
+
+describe 'Avatar Search Index Page' do
+  it 'allows users to select a nation and reroutes to a search page that shows all members of selected nation' do
+    visit root_path
+
+    select 'Fire Nation', from: :nation
+    click_on 'Search for Members'
+
+    expect(current_path).to eq(search_path)
+    expect(characters.count).to eq()
+  end
+end
